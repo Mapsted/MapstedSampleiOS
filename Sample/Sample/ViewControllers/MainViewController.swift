d75edcff54fc8121a058154b120fe4d47f301df5
Daniel Teklegiorgis <d.teklegiorgis@mapsted.com>
2021-09-14 22:03:46 -0400

    Update to remove file

--- Sample/Sample/ViewControllers/MainViewController.swift
+++ Sample/Sample/ViewControllers/MainViewController.swift
@@ -11,8 +11,6 @@ import MapstedCore
 import MapstedMap
 import MapstedMapUi
 
-#error(" A Mapsted license file is required in order to run the sample app. If you do not have a license, please contact https://mapsted.com/contact-us to get the license first.")
-
 class MainViewController : UIViewController {
     
     private var containerVC: ContainerViewController?
